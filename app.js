d58596fce36e66c7092a9cd13537ddcd6c7d7411
Lev Myshkin <student.lev.myshkin@gmail.com>
2023-04-22 14:45:29 +0300

    fix(basic-3.4): replace selectors by tags to classes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton=document.querySelector(".add");//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -18,9 +18,10 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
   var listItem=document.createElement("li");
-
+  listItem.classList.add("tasks-items");
   //input (checkbox)
   var checkBox=document.createElement("input");//checkbx
+  checkBox.classList.add("checkbox");
   //label
   var label=document.createElement("label");//label
   //input (text)
@@ -31,17 +32,19 @@ var createNewTaskElement=function(taskString){
   //button.delete
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
+  deleteButtonImg.classList.add("delete-img")
 
   label.innerText=taskString;
-  label.className='task';
+  label.classList.add("task");
 
   //Each elements, needs appending
   checkBox.type="checkbox";
   editInput.type="text";
-  editInput.className="task";
+  editInput.classList.add("task");
+  editInput.classList.add("task-input");
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.classList.add("edit");
 
   deleteButton.className="delete";
   deleteButtonImg.src='./remove.svg';
